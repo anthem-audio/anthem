afc5f89438ece6b4f1f7206365992ef6b11bb244
joshuawade <wadej@southern.edu>
2021-10-20 11:45:47 -0400

    License formatting

--- lib/widgets/basic/panel.dart
+++ lib/widgets/basic/panel.dart
@@ -1,20 +1,20 @@
 /*
-    Copyright (C) 2021 Joshua Wade
+  Copyright (C) 2021 Joshua Wade
 
-    This file is part of Anthem.
+  This file is part of Anthem.
 
-    Anthem is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  Anthem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    Anthem is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-    General Public License for more details.
+  Anthem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Anthem. If not, see <https://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with Anthem. If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:flutter/services.dart';
