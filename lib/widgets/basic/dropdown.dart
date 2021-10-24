57b7f126c321bdfead6cda695d29a20e489b5e58
joshuawade <wadej@southern.edu>
2021-10-24 00:43:46 -0400

    Finish dropdown cod

--- lib/widgets/basic/dropdown.dart
+++ lib/widgets/basic/dropdown.dart
@@ -25,26 +25,63 @@ import 'button.dart';
 import 'menu/menu.dart';
 import 'menu/menu_model.dart';
 
-class Dropdown extends StatelessWidget {
+class Dropdown extends StatefulWidget {
   double? width;
   double? height;
+  String? selectedID;
+  List<DropdownItem> items;
 
   Dropdown({
     Key? key,
     this.width,
     this.height,
+    this.selectedID,
+    this.items = const [],
   }) : super(key: key);
+
+  @override
+  State<Dropdown> createState() => _DropdownState();
+}
+
+class _DropdownState extends State<Dropdown> {
+  String? localSelectedID;
 
   @override
   Widget build(BuildContext context) {
     final menuController = MenuController();
+    final selectedID = widget.selectedID ?? localSelectedID;
 
     return Menu(
         menuController: menuController,
-        menuDef: MenuDef(children: [MenuItem(text: "New pattern")]),
+        menuDef: MenuDef(
+          children: widget.items
+                  .map<GenericMenuItem>((item) => MenuItem(
+                      text: item.name,
+                      onSelected: () {
+                        setState(() {
+                          localSelectedID = item.id;
+                        });
+                      }))
+                  .toList() +
+              [
+                Separator(),
+                MenuItem(
+                  text: "(none)",
+                  onSelected: () {
+                    setState(() {
+                      localSelectedID = null;
+                    });
+                  },
+                )
+              ],
+        ),
+        alignment: MenuAlignment.BottomLeft,
         child: Button(
-          width: this.width,
-          height: this.height,
+          onPress: () {
+            menuController.open?.call();
+          },
+          width: this.widget.width,
+          height: this.widget.height,
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 9),
             child: Row(
@@ -52,7 +89,10 @@ class Dropdown extends StatelessWidget {
               children: [
                 Expanded(
                   child: Text(
-                    "asdf",
+                    widget.items
+                        .firstWhere((element) => element.id == selectedID,
+                            orElse: () => DropdownItem(id: "", name: "(none)"))
+                        .name,
                     style: TextStyle(
                       color: Theme.text.main,
                       overflow: TextOverflow.ellipsis,
@@ -68,4 +108,12 @@ class Dropdown extends StatelessWidget {
           ),
         ));
   }
+}
+
+@immutable
+class DropdownItem {
+  final String id;
+  final String name;
+
+  DropdownItem({required this.id, required this.name});
 }
