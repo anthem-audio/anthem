a31abbccbe24bb0b433b8f8da84d4be9abacb13b
joshuawade <6700184+01jw@users.noreply.github.com>
2021-11-16 23:05:07 -0500

    Build anthem_engine in rid_build.rs and include engine executable in assets

--- rid_build.rs
+++ rid_build.rs
@@ -1,7 +1,9 @@
 use rid_build::{build, BuildConfig, BuildTarget, FlutterConfig, FlutterPlatform, Project};
-use std::env;
+use std::{env, fs, path::Path, process::Command};
 
 fn main() {
+    build_anthem_engine();
+
     let crate_dir = env::var("CARGO_MANIFEST_DIR")
         .expect("Missing CARGO_MANIFEST_DIR, please run this via 'cargo run'");
 
@@ -36,4 +38,53 @@ fn main() {
         workspace_root: Some(&workspace_dir),
     };
     build(&build_config).expect("Build failed");
+}
+
+fn build_anthem_engine() {
+    fs::create_dir("./build").ok();
+    fs::create_dir("./target").ok();
+
+    let crate_dir = env::var("CARGO_MANIFEST_DIR")
+        .expect("Missing CARGO_MANIFEST_DIR, please run this via 'cargo run'");
+    let engine_repo_dir_path = Path::new(&crate_dir).join("build").join("anthem_engine");
+    let engine_repo_dir = engine_repo_dir_path.to_str().unwrap();
+    let engine_cargo_file_path = engine_repo_dir_path.join("Cargo.toml");
+    let engine_cargo_file = engine_cargo_file_path.to_str().unwrap();
+    let engine_out_dir_path = Path::new(&crate_dir).join("assets").join("build");
+    let engine_out_dir = engine_out_dir_path.to_str().unwrap();
+
+    let clone_result = Command::new("git")
+        .args([
+            "clone",
+            "https://github.com/SecondFlight/anthem-engine.git",
+            engine_repo_dir,
+        ])
+        .output()
+        .unwrap();
+    let clone_result_str = String::from_utf8_lossy(&clone_result.stderr);
+
+    if clone_result_str.starts_with("fatal") {
+        Command::new("git")
+            .args(["-C", engine_repo_dir, "pull"])
+            .status()
+            .unwrap();
+    }
+
+    Command::new("cargo")
+        .args([
+            "+nightly",
+            "build",
+            "--manifest-path",
+            engine_cargo_file,
+            // Hack: output to a predictable directory, then set an environment
+            // variable. This won't work for the final build.
+            "--out-dir",
+            engine_out_dir,
+            "-Z",
+            "unstable-options",
+        ])
+        .output()
+        .unwrap();
+
+    env::set_var("ANTHEM_ENGINE_DIR", engine_out_dir);
 }
