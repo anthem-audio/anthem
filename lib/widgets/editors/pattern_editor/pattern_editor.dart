356bfdd9790c4e633c9bee6e6a57e9177f34efd7
joshuawade <SecondFlight@users.noreply.github.com>
2021-12-01 21:48:31 -0500

    Recompute scroll on resize in pattern editor

--- lib/widgets/editors/pattern_editor/pattern_editor.dart
+++ lib/widgets/editors/pattern_editor/pattern_editor.dart
@@ -24,6 +24,7 @@ import 'package:anthem/widgets/basic/button.dart';
 import 'package:anthem/widgets/basic/dropdown.dart';
 import 'package:anthem/widgets/basic/menu/menu.dart';
 import 'package:anthem/widgets/basic/menu/menu_model.dart';
+import 'package:anthem/widgets/basic/scroll/scrollbar.dart';
 import 'package:anthem/widgets/editors/pattern_editor/pattern_editor_cubit.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -41,6 +42,7 @@ class PatternEditor extends StatefulWidget {
 
 class _PatternEditorState extends State<PatternEditor> {
   double nextHue = 0;
+  ScrollController verticalScrollController = ScrollController();
 
   Color getColor() {
     final color = HSLColor.fromAHSL(1, nextHue, 0.33, 0.5).toColor();
@@ -54,59 +56,67 @@ class _PatternEditorState extends State<PatternEditor> {
         builder: (context, state) {
       final menuController = MenuController();
 
-      return Background(
-        type: BackgroundType.dark,
-        borderRadius: const BorderRadius.all(
-          Radius.circular(2),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(6.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Menu(
-                    menuController: menuController,
-                    menuDef: MenuDef(children: [
-                      MenuItem(
-                          text: "New pattern",
-                          onSelected: () {
-                            context.read<PatternEditorCubit>().addPattern(
-                                "Pattern ${(Random()).nextInt(100).toString()}");
-                          })
-                    ]),
-                    child: Button(
-                      width: 28,
+      return NotificationListener<SizeChangedLayoutNotification>(
+        onNotification: (notification) {
+          WidgetsBinding.instance?.addPostFrameCallback((_) {
+            verticalScrollController.position.notifyListeners();
+          });
+          return true;
+        },
+        child: SizeChangedLayoutNotifier(
+            child: Background(
+          type: BackgroundType.dark,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(2),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Menu(
+                      menuController: menuController,
+                      menuDef: MenuDef(children: [
+                        MenuItem(
+                            text: "New pattern",
+                            onSelected: () {
+                              context.read<PatternEditorCubit>().addPattern(
+                                  "Pattern ${(Random()).nextInt(100).toString()}");
+                            })
+                      ]),
+                      child: Button(
+                        width: 28,
+                        height: 28,
+                        iconPath: "assets/icons/file/kebab.svg",
+                        showMenuIndicator: true,
+                        onPress: () {
+                          menuController.open?.call();
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    Dropdown(
+                      width: 169,
                       height: 28,
-                      iconPath: "assets/icons/file/kebab.svg",
-                      showMenuIndicator: true,
-                      onPress: () {
-                        menuController.open?.call();
+                      items: state.patternList.map(
+                        (item) {
+                          return DropdownItem(
+                            id: item.id.toString(),
+                            name: item.name,
+                          );
+                        },
+                      ).toList(),
+                      selectedID: state.activePatternID.toString(),
+                      onChanged: (idStr) {
+                        final id = idStr == null ? 0 : int.parse(idStr);
+                        context.read<PatternEditorCubit>().setActivePattern(id);
                       },
                     ),
-                  ),
-                  const SizedBox(width: 4),
-                  Dropdown(
-                    width: 169,
-                    height: 28,
-                    items: state.patternList.map(
-                      (item) {
-                        return DropdownItem(
-                          id: item.id.toString(),
-                          name: item.name,
-                        );
-                      },
-                    ).toList(),
-                    selectedID: state.activePatternID.toString(),
-                    onChanged: (idStr) {
-                      final id = idStr == null ? 0 : int.parse(idStr);
-                      context.read<PatternEditorCubit>().setActivePattern(id);
-                    },
-                  ),
-                  const Expanded(child: SizedBox()),
-                  Button(
+                    const Expanded(child: SizedBox()),
+                    Button(
                       width: 28,
                       height: 28,
                       iconPath: "assets/icons/pattern_editor/add-audio.svg",
@@ -115,9 +125,10 @@ class _PatternEditorState extends State<PatternEditor> {
                               "Instrument ${(Random()).nextInt(100).toString()}",
                               getColor(),
                             );
-                      }),
-                  const SizedBox(width: 4),
-                  Button(
+                      },
+                    ),
+                    const SizedBox(width: 4),
+                    Button(
                       width: 28,
                       height: 28,
                       iconPath:
@@ -127,71 +138,80 @@ class _PatternEditorState extends State<PatternEditor> {
                               "Controller ${(Random()).nextInt(100).toString()}",
                               getColor(),
                             );
-                      }),
-                ],
-              ),
-              const SizedBox(height: 4),
-              Expanded(
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                      child: Background(
-                        type: BackgroundType.light,
-                        border: Border.all(color: Theme.panel.border),
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(2)),
-                        child: SingleChildScrollView(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5),
-                            child: Column(
-                                children:
-                                    state.generatorIDList.map<Widget>((id) {
-                              final instrument = state.instruments[id];
-                              final controller = state.controllers[id];
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 4),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Expanded(
+                        child: Background(
+                          type: BackgroundType.light,
+                          border: Border.all(color: Theme.panel.border),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(2)),
+                          child: SingleChildScrollView(
+                            controller: verticalScrollController,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 5),
+                              child: SizeChangedLayoutNotifier(
+                                child: Column(
+                                    children:
+                                        state.generatorIDList.map<Widget>((id) {
+                                  final instrument = state.instruments[id];
+                                  final controller = state.controllers[id];
 
-                              // TODO: provide type to child
-                              if (instrument != null) {
-                                return BlocProvider(
-                                  create: (context) => GeneratorRowCubit(
-                                    projectID: state.projectID,
-                                    patternID: state.activePatternID,
-                                    generatorID: id,
-                                  ),
-                                  child: const GeneratorRow(),
-                                );
-                              }
+                                  // TODO: provide type to child
+                                  if (instrument != null) {
+                                    return BlocProvider(
+                                      create: (context) => GeneratorRowCubit(
+                                        projectID: state.projectID,
+                                        patternID: state.activePatternID,
+                                        generatorID: id,
+                                      ),
+                                      child: const GeneratorRow(),
+                                    );
+                                  }
 
-                              if (controller != null) {
-                                return Padding(
-                                  padding: const EdgeInsets.only(bottom: 1),
-                                  child: BlocProvider(
-                                    create: (context) => GeneratorRowCubit(
-                                      projectID: state.projectID,
-                                      patternID: state.activePatternID,
-                                      generatorID: id,
-                                    ),
-                                    child: const GeneratorRow(),
-                                  ),
-                                );
-                              }
+                                  if (controller != null) {
+                                    return Padding(
+                                      padding: const EdgeInsets.only(bottom: 1),
+                                      child: BlocProvider(
+                                        create: (context) => GeneratorRowCubit(
+                                          projectID: state.projectID,
+                                          patternID: state.activePatternID,
+                                          generatorID: id,
+                                        ),
+                                        child: const GeneratorRow(),
+                                      ),
+                                    );
+                                  }
 
-                              throw Error();
-                            }).toList()),
+                                  throw Error();
+                                }).toList()),
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(width: 4),
-                    const SizedBox(width: 17),
-                  ],
+                      const SizedBox(width: 4),
+                      Scrollbar(
+                        controller: verticalScrollController,
+                        crossAxisSize: 17,
+                        direction: ScrollbarDirection.vertical,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 4),
-              const SizedBox(height: 17),
-            ],
+                const SizedBox(height: 4),
+                const SizedBox(height: 17),
+              ],
+            ),
           ),
-        ),
+        )),
       );
     });
   }
