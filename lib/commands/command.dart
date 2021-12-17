c58a887e380d3ee4c1caa025abea3bdad7fac4e0
joshuawade <6700184+01jw@users.noreply.github.com>
2021-12-17 01:00:44 -0500

    Enumerate supported state changes

--- lib/commands/command.dart
+++ lib/commands/command.dart
@@ -17,7 +17,7 @@
   along with Anthem. If not, see <https://www.gnu.org/licenses/>.
 */
 
-import 'package:anthem/commands/state_change.dart';
+import 'package:anthem/commands/state_changes.dart';
 
 abstract class Command {
   List<StateChange> execute();
