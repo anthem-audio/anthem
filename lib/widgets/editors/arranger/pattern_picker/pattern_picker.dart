60aaf447a25d2c0da98bf0c24fa45baad6cc2a06
joshuawade <6700184+01jw@users.noreply.github.com>
2022-03-22 21:55:20 -0400

    Add clip widget shell and show one clip per pattern

--- lib/widgets/editors/arranger/pattern_picker/pattern_picker.dart
+++ lib/widgets/editors/arranger/pattern_picker/pattern_picker.dart
@@ -17,20 +17,76 @@
   along with Anthem. If not, see <https://www.gnu.org/licenses/>.
 */
 
+import 'package:anthem/widgets/basic/clip/clip.dart' as anthem_clip;
 import 'package:anthem/widgets/editors/arranger/pattern_picker/pattern_picker_cubit.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../theme.dart';
+import '../../../basic/scroll/scrollbar.dart';
+
 class PatternPicker extends StatelessWidget {
-  const PatternPicker({Key? key}) : super(key: key);
+  final ScrollController scrollController = ScrollController();
+
+  PatternPicker({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<PatternPickerCubit, PatternPickerState>(
       builder: (context, state) {
-        return Container(
-          color: const Color(0xFFFF00FF),
-          child: Text(state.patterns.length.toString()),
+        return NotificationListener<SizeChangedLayoutNotification>(
+          onNotification: (notification) {
+            WidgetsBinding.instance?.addPostFrameCallback((_) {
+              scrollController.position.notifyListeners();
+            });
+            return true;
+          },
+          child: SizeChangedLayoutNotifier(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      border: Border.all(
+                        color: Theme.panel.border,
+                        width: 1,
+                      ),
+                      color: Theme.panel.accentDark,
+                    ),
+                    // clipBehavior: Clip.antiAlias,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(4),
+                      child: SingleChildScrollView(
+                        controller: scrollController,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: state.patterns
+                              .map(
+                                (pattern) => Padding(
+                                  padding: const EdgeInsets.only(bottom: 1),
+                                  child: SizedBox(
+                                    height: 30,
+                                    child: anthem_clip.Clip(),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 4),
+                Scrollbar(
+                  controller: scrollController,
+                  crossAxisSize: 17,
+                  direction: ScrollbarDirection.vertical,
+                ),
+              ],
+            ),
+          ),
         );
       },
     );
