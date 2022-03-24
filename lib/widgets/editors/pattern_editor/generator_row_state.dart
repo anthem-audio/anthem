6c3800c0138bd494ac0ec9eadcd43ec9c80414dd
joshuawade <6700184+01jw@users.noreply.github.com>
2022-03-23 23:10:36 -0400

    Add freezed to generator_row_state.dart

--- lib/widgets/editors/pattern_editor/generator_row_state.dart
+++ lib/widgets/editors/pattern_editor/generator_row_state.dart
@@ -19,53 +19,13 @@
 
 part of 'generator_row_cubit.dart';
 
-@immutable
-class GeneratorRowState {
-  final int generatorID;
-  final int projectID;
-  final Optional<PatternModel> pattern;
-  final Color color;
-  final List<ClipNoteModel> clipNotes;
-
-  const GeneratorRowState({
-    required this.generatorID,
-    required this.projectID,
-    required this.pattern,
-    required this.color,
-    this.clipNotes = const [],
-  });
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is GeneratorRowState &&
-          other.generatorID == generatorID &&
-          other.projectID == projectID &&
-          other.pattern == pattern &&
-          other.color == color &&
-          other.clipNotes == clipNotes;
-
-  @override
-  int get hashCode =>
-      generatorID.hashCode ^
-      projectID.hashCode ^
-      pattern.hashCode ^
-      color.hashCode ^
-      clipNotes.hashCode;
-
-  GeneratorRowState copyWith({
-    int? generatorID,
-    int? projectID,
-    Optional<PatternModel>? pattern,
-    Color? color,
-    List<ClipNoteModel>? clipNotes,
-  }) {
-    return GeneratorRowState(
-      generatorID: generatorID ?? this.generatorID,
-      projectID: projectID ?? this.projectID,
-      pattern: pattern ?? this.pattern,
-      color: color ?? this.color,
-      clipNotes: clipNotes ?? this.clipNotes,
-    );
-  }
+@freezed
+abstract class GeneratorRowState with _$GeneratorRowState {
+  factory GeneratorRowState({
+    required int generatorID,
+    required int projectID,
+    required int? patternID,
+    required Color color,
+    @Default([]) List<ClipNoteModel> clipNotes,
+  }) = _GeneratorRowState;
 }
