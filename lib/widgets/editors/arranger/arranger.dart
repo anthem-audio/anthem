a8dd7bddfff4f3e596c687c1a5f6c1b45b4feec2
joshuawade <6700184+01jw@users.noreply.github.com>
2022-03-31 14:28:34 -0400

    Add timeline to arranger

--- lib/widgets/editors/arranger/arranger.dart
+++ lib/widgets/editors/arranger/arranger.dart
@@ -23,6 +23,12 @@ import 'package:anthem/widgets/editors/arranger/pattern_picker/pattern_picker.da
 import 'package:anthem/widgets/editors/arranger/pattern_picker/pattern_picker_cubit.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+
+import '../../../theme.dart';
+import '../piano_roll/helpers.dart';
+import '../shared/timeline.dart';
+import '../shared/timeline_cubit.dart';
 
 class Arranger extends StatefulWidget {
   const Arranger({Key? key}) : super(key: key);
@@ -39,62 +45,93 @@ class _ArrangerState extends State<Arranger> {
   Widget build(BuildContext context) {
     return BlocBuilder<ArrangerCubit, ArrangerState>(
       builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.all(6),
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => TimeView(0, 3072)),
+          ],
+          child: Padding(
+            padding: const EdgeInsets.all(6),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 26),
+                const SizedBox(height: 4),
+                Expanded(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: SizedBox(
+                          width: 126,
+                          child: BlocProvider<PatternPickerCubit>(
+                            create: (context) =>
+                                PatternPickerCubit(projectID: state.projectID),
+                            child: PatternPicker(),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 6),
+                      const Expanded(
+                        child: ArrangerContent(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+// Actual content view of the arranger (timeline + clips + etc)
+class ArrangerContent extends StatelessWidget {
+  const ArrangerContent({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ArrangerCubit, ArrangerState>(builder: (context, state) {
+      return Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Theme.panel.border),
+          borderRadius: BorderRadius.circular(4),
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(4),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              SizedBox(height: 26),
-              const SizedBox(height: 4),
-              Expanded(
+              SizedBox(
+                height: 44,
                 child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 15),
-                      child: SizedBox(
-                        width: 126,
-                        child: BlocProvider<PatternPickerCubit>(
-                          create: (context) =>
-                              PatternPickerCubit(projectID: state.projectID),
-                          child: PatternPicker(),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 6),
+                    const SizedBox(width: 130),
+                    Container(width: 1, color: Theme.panel.border),
                     Expanded(
-                      child: Container(
-                        color: const Color(0x11FFFFFF),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Row(children: [
-                              SizedBox(width: 100, child: Text("x: $x,")),
-                              SizedBox(width: 100, child: Text("y: $y"))
-                            ]),
-                            Expanded(
-                              child: ControlMouseHandler(
-                                child:
-                                    Container(color: const Color(0xFFFABCDE)),
-                                onChange: (e) {
-                                  setState(() {
-                                    x += e.delta.dx;
-                                    y += e.delta.dy;
-                                  });
-                                },
-                              ),
-                            ),
-                          ],
+                      child: BlocProvider<TimelineCubit>(
+                        create: (context) => TimelineCubit(
+                          projectID: state.projectID,
+                          timelineType: TimelineType.arrangerTimeline,
                         ),
+                        child: const Timeline(),
                       ),
                     ),
                   ],
                 ),
               ),
+              Container(height: 1, color: Theme.panel.border),
+              Expanded(
+                child: Container(
+                  color: const Color(0x11FFFFFF),
+                ),
+              ),
             ],
           ),
-        );
-      },
-    );
+        ),
+      );
+    });
   }
 }
