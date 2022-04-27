28120bfe36cf0f066a9fce7c39bf54f1aca35bca
Joshua Wade <6700184+01jw@users.noreply.github.com>
2022-04-27 16:58:57 -0400

    Add project footer shell

---
+++ lib/widgets/project/project_footer.dart
@@ -0,0 +1,36 @@
+/*
+  Copyright (C) 2022 Joshua Wade
+
+  This file is part of Anthem.
+
+  Anthem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Anthem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Anthem. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:anthem/theme.dart';
+import 'package:flutter/widgets.dart';
+
+class ProjectFooter extends StatelessWidget {
+  const ProjectFooter({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 42,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: Theme.panel.main,
+      ),
+    );
+  }
+}
