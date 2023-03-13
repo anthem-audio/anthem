ab3c1fbd1b13bb41acb924f0ff88a63851c3d37d
Joshua Wade <SecondFlight@users.noreply.github.com>
2023-03-13 11:00:19 -0400

    Remove remaining cubits

--- lib/widgets/project_details/time_signature_change_detail_view.dart
+++ lib/widgets/project_details/time_signature_change_detail_view.dart
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2022 Joshua Wade
+  Copyright (C) 2022 - 2023 Joshua Wade
 
   This file is part of Anthem.
 
@@ -19,66 +19,96 @@
 
 import 'dart:math';
 
+import 'package:anthem/commands/timeline_commands.dart';
+import 'package:anthem/model/project.dart';
+import 'package:anthem/model/shared/time_signature.dart';
 import 'package:anthem/widgets/basic/dropdown.dart';
-import 'package:anthem/widgets/project_details/time_signature_change_detail_view_cubit.dart';
 import 'package:anthem/widgets/project_details/widgets.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
-class TimeSignatureChangeDetailView extends StatelessWidget {
+class TimeSignatureChangeDetailView extends StatelessObserverWidget {
   const TimeSignatureChangeDetailView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<TimeSignatureChangeDetailViewCubit,
-        TimeSignatureChangeDetailViewState>(
-      builder: (context, state) {
-        final cubit = Provider.of<TimeSignatureChangeDetailViewCubit>(context);
+    final project = Provider.of<ProjectModel>(context);
+    final detailView =
+        project.selectedDetailView as TimeSignatureChangeDetailViewKind;
 
-        return Column(
+    late TimeSignatureChangeModel timeSignatureChange;
+    if (detailView.arrangementID != null) {
+      throw UnimplementedError(
+          "Time signature changes in arrangements are not supported yet.");
+    } else if (detailView.patternID != null) {
+      timeSignatureChange = project
+          .song.patterns[detailView.patternID]!.timeSignatureChanges
+          .firstWhere((change) => change.id == detailView.changeID);
+    } else {
+      throw Exception(
+          "Invalid TimeSignatureChangeDetailViewKind - it should specify an arrangement ID or pattern ID, but it specified neither.");
+    }
+
+    return Column(
+      children: [
+        Section(
+          title: "TIME SIGNATURE CHANGE",
           children: [
-            Section(
-              title: "TIME SIGNATURE CHANGE",
-              children: [
-                Dropdown(
-                  height: 26,
-                  allowNoSelection: false,
-                  selectedID: state.numerator.toString(),
-                  items: List.generate(
-                    32,
-                    (index) => DropdownItem(
-                      id: (index + 1).toString(),
-                      name: (index + 1).toString(),
-                    ),
-                  ),
-                  onChanged: (id) {
-                    cubit.setNumerator(int.parse(id!));
-                  },
+            Dropdown(
+              height: 26,
+              allowNoSelection: false,
+              selectedID:
+                  timeSignatureChange.timeSignature.numerator.toString(),
+              items: List.generate(
+                32,
+                (index) => DropdownItem(
+                  id: (index + 1).toString(),
+                  name: (index + 1).toString(),
                 ),
-                const SizedBox(height: 4),
-                Dropdown(
-                  height: 26,
-                  allowNoSelection: false,
-                  selectedID: state.denominator.toString(),
-                  items: List.generate(
-                    6,
-                    (index) {
-                      final value = pow(2, index).toString();
-                      return DropdownItem(id: value, name: value);
-                    },
+              ),
+              onChanged: (id) {
+                project.execute(
+                  SetTimeSignatureNumeratorCommand(
+                    project: project,
+                    patternID: detailView.patternID,
+                    arrangementID: detailView.arrangementID,
+                    changeID: detailView.changeID,
+                    numerator: int.parse(id!),
                   ),
-                  onChanged: (id) {
-                    cubit.setDenominator(int.parse(id!));
-                  },
-                ),
-              ],
+                );
+              },
             ),
             const SizedBox(height: 4),
-            const Expanded(child: SizedBox()),
+            Dropdown(
+              height: 26,
+              allowNoSelection: false,
+              selectedID:
+                  timeSignatureChange.timeSignature.numerator.toString(),
+              items: List.generate(
+                6,
+                (index) {
+                  final value = pow(2, index).toString();
+                  return DropdownItem(id: value, name: value);
+                },
+              ),
+              onChanged: (id) {
+                project.execute(
+                  SetTimeSignatureDenominatorCommand(
+                    project: project,
+                    patternID: detailView.patternID,
+                    arrangementID: detailView.arrangementID,
+                    changeID: detailView.changeID,
+                    denominator: int.parse(id!),
+                  ),
+                );
+              },
+            ),
           ],
-        );
-      },
+        ),
+        const SizedBox(height: 4),
+        const Expanded(child: SizedBox()),
+      ],
     );
   }
 }
