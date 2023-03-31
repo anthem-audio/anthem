d20efc5b78b5ef3256c81a9f178fc732308120af
Joshua Wade <6700184+01jw@users.noreply.github.com>
2023-03-31 16:38:28 -0400

    Support scroll to change selected dropdown item

--- lib/widgets/basic/dropdown.dart
+++ lib/widgets/basic/dropdown.dart
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2021 - 2022 Joshua Wade
+  Copyright (C) 2021 - 2023 Joshua Wade
 
   This file is part of Anthem.
 
@@ -17,6 +17,7 @@
   along with Anthem. If not, see <https://www.gnu.org/licenses/>.
 */
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/widgets.dart';
 import 'package:collection/collection.dart';
 
@@ -65,33 +66,57 @@ class _DropdownState extends State<Dropdown> {
     );
 
     return Menu(
-        menuController: menuController,
-        menuDef: MenuDef(
-          children: widget.items
-                  .map<GenericMenuItem>((item) => AnthemMenuItem(
-                      text: item.name,
-                      onSelected: () {
-                        setState(() {
-                          localSelectedID = item.id;
-                        });
-                        widget.onChanged?.call(item.id);
-                      }))
-                  .toList() +
-              (!widget.allowNoSelection
-                  ? []
-                  : [
-                      widget.items.isNotEmpty ? Separator() : null,
-                      AnthemMenuItem(
-                        text: '(none)',
-                        onSelected: () {
-                          setState(() {
-                            localSelectedID = null;
-                          });
-                          widget.onChanged?.call(null);
-                        },
-                      )
-                    ].whereNotNull().toList()),
-        ),
+      menuController: menuController,
+      menuDef: MenuDef(
+        children: widget.items
+                .map<GenericMenuItem>(
+                  (item) => AnthemMenuItem(
+                    text: item.name,
+                    onSelected: () => select(item.id),
+                  ),
+                )
+                .toList() +
+            (!widget.allowNoSelection
+                ? []
+                : [
+                    widget.items.isNotEmpty ? Separator() : null,
+                    AnthemMenuItem(
+                      text: '(none)',
+                      onSelected: () => select(null),
+                    )
+                  ].whereNotNull().toList()),
+      ),
+      child: Listener(
+        onPointerSignal: (event) {
+          if (event is PointerScrollEvent) {
+            // How many items to scroll by on this event
+            final itemIndexDelta = (event.scrollDelta.dy / 100).ceil();
+
+            final selectedID = selectedItem.id;
+            var selectedIndex =
+                widget.items.indexWhere((element) => element.id == selectedID);
+
+            // If we didn't find it, then we can probably assume there is no
+            // selected item.
+            if (selectedIndex < 0) {
+              selectedIndex = widget.items.length;
+            }
+
+            var itemCount = widget.items.length;
+
+            if (widget.allowNoSelection) itemCount++;
+
+            var newIndex = (selectedIndex + itemIndexDelta) % itemCount;
+
+            if (newIndex < 0) newIndex += itemCount;
+
+            if (newIndex == widget.items.length) {
+              select(null);
+            } else {
+              select(widget.items[newIndex].id);
+            }
+          }
+        },
         child: Button(
           onPress: () {
             menuController.open?.call();
@@ -101,33 +126,16 @@ class _DropdownState extends State<Dropdown> {
           text: widget.showNameOnButton ? selectedItem.name : null,
           startIcon: selectedItem.icon,
           endIcon: Icons.arrowDown,
+        ),
+      ),
+    );
+  }
 
-          // child: Padding(
-          //   padding: const EdgeInsets.symmetric(horizontal: 9),
-          //   child: Row(
-          //     crossAxisAlignment: CrossAxisAlignment.center,
-          //     children: [
-          //       Expanded(
-          //         child: Text(
-          //           widget.items
-          //               .firstWhere((element) => element.id == selectedID,
-          //                   orElse: () =>
-          //                       const DropdownItem(id: "", name: "(none)"))
-          //               .name,
-          //           style: TextStyle(
-          //             color: Theme.text.main,
-          //             overflow: TextOverflow.ellipsis,
-          //           ),
-          //         ),
-          //       ),
-          //       SvgPicture.asset(
-          //         "assets/icons/small/arrow-down-selectbtn.svg",
-          //         color: Theme.text.main,
-          //       ),
-          //     ],
-          //   ),
-          // ),
-        ));
+  void select(String? id) {
+    setState(() {
+      localSelectedID = id;
+    });
+    widget.onChanged?.call(id);
   }
 }
 
