965a77bedddacaa25223365d1169107bbf7141d4
SecondFlight <6700184+01jw@users.noreply.github.com>
2023-06-22 00:22:01 -0400

    Add timeline to automation editor

--- lib/widgets/editors/automation_editor/automation_editor.dart
+++ lib/widgets/editors/automation_editor/automation_editor.dart
@@ -21,8 +21,12 @@ import 'package:anthem/widgets/basic/background.dart';
 import 'package:anthem/widgets/basic/button.dart';
 import 'package:anthem/widgets/basic/icon.dart';
 import 'package:anthem/widgets/editors/automation_editor/view_model.dart';
+import 'package:anthem/widgets/editors/shared/helpers/types.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
+import 'package:mobx/mobx.dart' as mobx;
+
+import '../shared/timeline/timeline.dart';
 
 class AutomationEditor extends StatefulWidget {
   const AutomationEditor({super.key});
@@ -36,7 +40,7 @@ class AutomationEditorState extends State<AutomationEditor> {
 
   @override
   Widget build(BuildContext context) {
-    viewModel ??= AutomationEditorViewModel();
+    viewModel ??= AutomationEditorViewModel(timeView: TimeRange(0, 3072));
 
     return Provider.value(
       value: viewModel!,
@@ -87,9 +91,86 @@ class _AutomationEditorContent extends StatefulWidget {
       _AutomationEditorContentState();
 }
 
-class _AutomationEditorContentState extends State<_AutomationEditorContent> {
+class _AutomationEditorContentState extends State<_AutomationEditorContent>
+    with TickerProviderStateMixin {
+  // Fields for time view animation
+
+  late final AnimationController _timeViewAnimationController =
+      AnimationController(
+    duration: const Duration(milliseconds: 250),
+    vsync: this,
+  );
+
+  double _lastTimeViewStart = 0;
+  double _lastTimeViewEnd = 1;
+
+  late final Tween<double> _timeViewStartTween =
+      Tween<double>(begin: _lastTimeViewStart, end: _lastTimeViewStart);
+  late final Tween<double> _timeViewEndTween =
+      Tween<double>(begin: _lastTimeViewEnd, end: _lastTimeViewEnd);
+
+  late final Animation<double> _timeViewStartAnimation =
+      _timeViewStartTween.animate(
+    CurvedAnimation(
+      parent: _timeViewAnimationController,
+      curve: Curves.easeOutExpo,
+    ),
+  );
+  late final Animation<double> _timeViewEndAnimation =
+      _timeViewEndTween.animate(
+    CurvedAnimation(
+      parent: _timeViewAnimationController,
+      curve: Curves.easeOutExpo,
+    ),
+  );
+
+  mobx.ReactionDisposer? animationTweenUpdaterDisposer;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final viewModel = Provider.of<AutomationEditorViewModel>(context);
+
+    // Updates the time view animation if the time view has changed
+    if (viewModel.timeView.start != _lastTimeViewStart ||
+        viewModel.timeView.end != _lastTimeViewEnd) {
+      _timeViewStartTween.begin = _timeViewStartAnimation.value;
+      _timeViewEndTween.begin = _timeViewEndAnimation.value;
+
+      _timeViewAnimationController.reset();
+
+      _timeViewStartTween.end = viewModel.timeView.start;
+      _timeViewEndTween.end = viewModel.timeView.end;
+
+      _timeViewAnimationController.forward();
+
+      _lastTimeViewStart = viewModel.timeView.start;
+      _lastTimeViewEnd = viewModel.timeView.end;
+    }
+
+    // Updates the animations whenever the vertical scroll position changes.
+    animationTweenUpdaterDisposer ??= mobx.autorun((p0) {
+      viewModel.timeView.start;
+      viewModel.timeView.end;
+
+      setState(() {});
+    });
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Provider.value(
+          value: viewModel.timeView,
+          child: SizedBox(
+            height: 21,
+            child: Timeline.pattern(
+              patternID: null,
+              timeViewStartAnimation: _timeViewStartAnimation,
+              timeViewEndAnimation: _timeViewEndAnimation,
+              timeViewAnimationController: _timeViewAnimationController,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
