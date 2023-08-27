6fe7849b52220a7ee82ab25e8287d55db3c7dbc7
SecondFlight <6700184+01jw@users.noreply.github.com>
2023-08-26 23:02:14 -0400

    Refactor point animation code to update declaratively

--- lib/widgets/editors/automation_editor/controller/automation_editor_controller.dart
+++ lib/widgets/editors/automation_editor/controller/automation_editor_controller.dart
@@ -26,11 +26,11 @@ import 'package:anthem/model/pattern/automation_point.dart';
 import 'package:anthem/model/project.dart';
 import 'package:anthem/widgets/basic/menu/menu_model.dart';
 import 'package:anthem/widgets/editors/automation_editor/view_model.dart';
-import 'package:anthem/widgets/editors/shared/canvas_annotation_set.dart';
 import 'package:anthem/widgets/editors/shared/helpers/time_helpers.dart';
 import 'package:anthem/widgets/editors/shared/helpers/types.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/gestures.dart';
+import 'package:mobx/mobx.dart';
 
 import '../automation_point_animation_tracker.dart';
 import '../events.dart';
@@ -48,6 +48,91 @@ class AutomationEditorController extends _AutomationEditorController
 class _AutomationEditorController {
   AutomationEditorViewModel viewModel;
   ProjectModel project;
+  late ReactionDisposer pointAnimationAutorunDisposer;
 
-  _AutomationEditorController({required this.viewModel, required this.project});
+  _AutomationEditorController(
+      {required this.viewModel, required this.project}) {
+    // This autorun updates the animation targets for automation points based on
+    // the current hovered item.
+    pointAnimationAutorunDisposer = autorun((_) {
+      final pattern = project.song.patterns[project.song.activePatternID];
+      if (pattern == null) return;
+
+      final automationLane =
+          pattern.automationLanes[project.activeAutomationGeneratorID];
+      if (automationLane == null) return;
+
+      // These will not be watched if there aren't any points, so we'll access
+      // them here.
+      viewModel.hoveredPointAnnotation;
+      viewModel.pressedPointAnnotation;
+
+      final visitedPointIds = <ID>{};
+
+      // We don't need to observe the whole list - we just care when the hovered
+      // and pressed point values change.
+      for (final point in automationLane.points.nonObservableInner) {
+        visitedPointIds.add(point.id);
+
+        for (final handleKind in HandleKind.values) {
+          final handle = viewModel.pointAnimationTracker.values[(
+            handleKind: handleKind,
+            id: point.id,
+          )];
+
+          var didUpdateHandle = false;
+
+          for (final (handleState, annotation) in [
+            (_HandleState.out, null),
+            (_HandleState.hovered, viewModel.hoveredPointAnnotation),
+            (_HandleState.pressed, viewModel.pressedPointAnnotation),
+          ]) {
+            if (annotation?.pointId == point.id &&
+                annotation?.kind == handleKind) {
+              if (handle != null) {
+                handle.setTarget(_getHandleTargetValue(handleState));
+                didUpdateHandle = true;
+              } else if (annotation != null) {
+                viewModel.pointAnimationTracker.addValue(
+                  id: point.id,
+                  handleKind: handleKind,
+                  value: AutomationPointAnimationValue(
+                    start: _getHandleTargetValue(_HandleState.out),
+                    target: _getHandleTargetValue(handleState),
+                    pointId: point.id,
+                    handleKind: handleKind,
+                    restPos: _getHandleTargetValue(_HandleState.out),
+                  ),
+                );
+              }
+            }
+          }
+
+          if (!didUpdateHandle) {
+            handle?.setTarget(_getHandleTargetValue(_HandleState.out));
+          }
+        }
+      }
+
+      // Remove any points in the animation list that aren't in the current
+      // automation lane.
+      for (final animationKey in viewModel.pointAnimationTracker.values.keys) {
+        if (!visitedPointIds.contains(animationKey.id)) {
+          viewModel.pointAnimationTracker.values.remove(animationKey);
+        }
+      }
+    });
+  }
+
+  void dispose() {
+    pointAnimationAutorunDisposer();
+  }
+}
+
+double _getHandleTargetValue(_HandleState state) {
+  return switch (state) {
+    _HandleState.out => 1,
+    _HandleState.hovered => automationPointHoveredSizeMultiplier,
+    _HandleState.pressed => automationPointPressedSizeMultiplier,
+  };
 }
