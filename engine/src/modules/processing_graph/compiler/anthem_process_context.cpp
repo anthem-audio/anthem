196f76cf0333ba07cd64c3ba5dfafdb702a37a2f
SecondFlight <SecondFlight@users.noreply.github.com>
2024-06-05 00:38:06 -0400

    Pull smoothing duration from parameter config

--- engine/src/modules/processing_graph/compiler/anthem_process_context.cpp
+++ engine/src/modules/processing_graph/compiler/anthem_process_context.cpp
@@ -51,8 +51,9 @@ AnthemProcessContext::AnthemProcessContext(std::shared_ptr<AnthemGraphNode> grap
 
   for (int i = 0; i < graphNode->controlInputs.size(); i++) {
     auto parameterValue = graphNode->parameters[i];
+    auto& parameterConfig = graphNode->processor->config.getParameter(i);
 
-    auto smoother = std::make_unique<LinearParameterSmoother>(parameterValue, 0.001f); // TODO: Make this configurable
+    auto smoother = std::make_unique<LinearParameterSmoother>(parameterValue, parameterConfig->smoothingDurationSeconds);
     parameterSmoothers.push_back(std::move(smoother));
   }
 }
