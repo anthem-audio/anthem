daa23aa4c8d12b179dc467cd77364ecde7d52c23
SecondFlight <SecondFlight@users.noreply.github.com>
2024-07-31 23:27:40 -0400

    Add a note about use-after-free risk for AnthemGraphNode::runtimeContext

--- engine/src/modules/processing_graph/topology/anthem_graph_node.h
+++ engine/src/modules/processing_graph/topology/anthem_graph_node.h
@@ -38,6 +38,27 @@ public:
 
   std::vector<float> parameters;
 
+  // This holds a pointer to the associated runtime context for this node, if
+  // one exists. This is used to send live control updates to the audio thread,
+  // via a vector of atomic floats.
+  //
+  // This doesn't risk a use-after-free as of this writing. The lifecycle of the
+  // object in this pointer is as follows:
+  //    1. When the graph is compiled, contexts are generated and owned by the
+  //       compilation result object. The runtimeContext field of each
+  //       AnthemGraphNode is also set during compilation.
+  //    2. After compilation is finished, the resulting object is sent to the
+  //       audio thread.
+  //    3. If and when a new graph is compiled, this field is updated with a
+  //       fresh context during the compilation and before the compiled result
+  //       is sent to the audio thread.
+  //    4. After the audio thread receives the new compilation result, it will
+  //       send back the old compilation result for the main thread to
+  //       deallocate.
+  //
+  // This field is always updated with a new pointer before the graph update is
+  // sent to the audio thread, and the old pointer will not be freed until this
+  // happens, so there is no risk of use-after-free.
   std::optional<AnthemProcessContext*> runtimeContext;
 
   static std::shared_ptr<AnthemGraphNode> create(std::unique_ptr<AnthemProcessor> processor);
