f39c06127bcaff4d6bf8e69ce3a942b535c25d6e
SecondFlight <6700184+01jw@users.noreply.github.com>
2024-08-26 00:39:52 -0400

    Add back code generation annotation for request and response messages

--- lib/engine_api/messages/messages.dart
+++ lib/engine_api/messages/messages.dart
@@ -35,22 +35,24 @@ class Heartbeat extends Request {}
 
 class HeartbeatReply extends Response {}
 
-// @AnthemModel(serializable: true)
-sealed class Request extends _Request /*with _$RequestAnthemModelMixin*/ {
+@AnthemModel(serializable: true)
+sealed class Request extends _Request with _$RequestAnthemModelMixin {
   Request();
 
-  // factory Request.fromJson_ANTHEM(Map<String, dynamic> json) => _$RequestAnthemModelMixin.fromJson_ANTHEM(json);
+  factory Request.fromJson_ANTHEM(Map<String, dynamic> json) =>
+      _$RequestAnthemModelMixin.fromJson_ANTHEM(json);
 }
 
 class _Request {
   late int id;
 }
 
-// @AnthemModel(serializable: true)
-sealed class Response extends _Response /*with _$ResponseAnthemModelMixin*/ {
+@AnthemModel(serializable: true)
+sealed class Response extends _Response with _$ResponseAnthemModelMixin {
   Response();
 
-  // factory Response.fromJson_ANTHEM(Map<String, dynamic> json) => _$ResponseAnthemModelMixin.fromJson_ANTHEM(json);
+  factory Response.fromJson_ANTHEM(Map<String, dynamic> json) =>
+      _$ResponseAnthemModelMixin.fromJson_ANTHEM(json);
 }
 
 class _Response {
