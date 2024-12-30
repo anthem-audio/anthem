6b906440c961e7b180feab01568fe428960acd10
SecondFlight <SecondFlight@users.noreply.github.com>
2024-12-29 21:33:39 -0500

    Don't check for build flag in test script

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,15 +1,12 @@
-# Check for --build option passed to the script
-if ($args -contains "--build") {
-  # Set the repository root as the current directory
-  $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-  Push-Location $scriptPath\..
+# Set the repository root as the current directory
+$scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+Push-Location $scriptPath\..
 
-  # Build test executable
-  .\engine\build_test.ps1
+# Build test executable
+.\engine\build_test.ps1
 
-  # Ensure that the assets directory exists
-  New-Item -ItemType Directory -Path ".\assets\engine" -Force | Out-Null
-}
+# Ensure that the assets directory exists
+New-Item -ItemType Directory -Path ".\assets\engine" -Force | Out-Null
 
 # Set test executable source path
 $platform = [System.Environment]::OSVersion.Platform
