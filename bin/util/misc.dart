2b6967fa92b817b6f8f95881d41f1333db4982c3
SecondFlight <SecondFlight@users.noreply.github.com>
2025-01-05 14:57:12 -0500

    Add anthem:cli command, and start work on anthem:cli build engine

---
+++ bin/util/misc.dart
@@ -0,0 +1,25 @@
+/*
+  Copyright (C) 2025 Joshua Wade
+
+  This file is part of Anthem.
+
+  Anthem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Anthem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Anthem. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'dart:io';
+
+Uri getPackageRootPath() {
+  final scriptPath = Platform.script;
+  return Uri.directory(scriptPath.pathSegments.takeWhile((s) => s != '.dart_tool').join('/'));
+}
