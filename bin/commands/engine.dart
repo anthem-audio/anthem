3e681f20e14ede4f33e56ec640300c719a73b75e
Joshua <SecondFlight@users.noreply.github.com>
2025-01-06 16:33:53 -0500

    anthem:cli: Copy engine release builds using the correct path

--- bin/commands/engine.dart
+++ bin/commands/engine.dart
@@ -155,7 +155,7 @@ to generate the files, then run this script again.''')
     print(Colorize('Copying engine binary to Flutter assets directory...')
       ..lightGreen());
     final engineBinaryPath = packageRootPath.resolve(
-        'engine/build/AnthemEngine_artefacts${argResults!['debug'] ? '/Debug' : ''}/AnthemEngine${Platform.isWindows ? '.exe' : ''}');
+        'engine/build/AnthemEngine_artefacts${argResults!['debug'] ? '/Debug' : '/Release'}/AnthemEngine${Platform.isWindows ? '.exe' : ''}');
     final flutterAssetsDirPath = packageRootPath.resolve('assets/engine/');
 
     // Create the engine directory in assets if it doesn't exist
