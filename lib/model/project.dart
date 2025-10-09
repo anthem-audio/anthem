7d6cb417cdb90991ad3e6e5e8762b942d93710d3
Joshua Wade <6700184+josh-audio@users.noreply.github.com>
2025-10-08 23:26:48 -0400

    Fix out of order initialization in ProjectModel

--- lib/model/project.dart
+++ lib/model/project.dart
@@ -190,7 +190,11 @@ abstract class _ProjectModel extends Hydratable with Store, AnthemModelBase {
   final engineID = getEngineID();
 
   @hide
-  late Engine engine;
+  late Engine engine = Engine(
+    engineID,
+    this as ProjectModel,
+    enginePathOverride: _enginePathOverride,
+  );
 
   @anthemObservable
   @hide
@@ -237,12 +241,6 @@ abstract class _ProjectModel extends Hydratable with Store, AnthemModelBase {
   /// This function is run after deserialization. It allows us to do some setup
   /// that the deserialization step can't do for us.
   void hydrate() {
-    engine = Engine(
-      engineID,
-      this as ProjectModel,
-      enginePathOverride: _enginePathOverride,
-    );
-
     engine.engineStateStream.listen((state) {
       (this as ProjectModel).engineState = state;
 
