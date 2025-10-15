f883967879185ff0316bf8ef6f4958364617ce86
Joshua Wade <6700184+josh-audio@users.noreply.github.com>
2025-10-14 21:50:10 -0400

    Adjust typography for dialog header

--- lib/widgets/basic/dialog/dialog_renderer.dart
+++ lib/widgets/basic/dialog/dialog_renderer.dart
@@ -117,8 +117,7 @@ class _DialogRendererState extends State<DialogRenderer>
                                     currentDialogTitle!,
                                     style: TextStyle(
                                       color: AnthemTheme.text.main,
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
                                     ),
                                   ),
                                 ),
