b77ccc5728c42892f9d27cdf44ea750e83937827
Joshua Wade <6700184+josh-audio@users.noreply.github.com>
2026-01-06 22:25:02 -0500

    Move track header group to track_header.dart

--- lib/widgets/editors/arranger/widgets/track_header.dart
+++ lib/widgets/editors/arranger/widgets/track_header.dart
@@ -18,9 +18,12 @@
 */
 
 import 'package:anthem/helpers/id.dart';
+import 'package:anthem/logic/service_registry.dart';
 import 'package:anthem/model/project.dart';
 import 'package:anthem/theme.dart';
+import 'package:anthem/widgets/basic/button.dart';
 import 'package:anthem/widgets/basic/hint/hint.dart';
+import 'package:anthem/widgets/basic/icon.dart';
 import 'package:anthem/widgets/editors/arranger/helpers.dart';
 import 'package:anthem/widgets/editors/arranger/view_model.dart';
 import 'package:flutter/widgets.dart';
@@ -261,6 +264,143 @@ class _TrackHeaderResizeHandleState extends State<TrackHeaderResizeHandle> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class TrackHeaders extends StatefulWidget {
+  final double verticalScrollPosition;
+
+  const TrackHeaders({super.key, required this.verticalScrollPosition});
+
+  @override
+  State<TrackHeaders> createState() => _TrackHeadersState();
+}
+
+class _TrackHeadersState extends State<TrackHeaders> {
+  @override
+  Widget build(BuildContext context) {
+    final viewModel = Provider.of<ArrangerViewModel>(context);
+    final project = Provider.of<ProjectModel>(context);
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final editorHeight = constraints.maxHeight;
+
+        return Observer(
+          builder: (context) {
+            List<Widget> headers = [];
+            List<Widget> resizeHandles = [];
+
+            double positionForAddButton = double.nan;
+            var lastTrackWasSendTrack = false;
+            var lastTrackBottom = 0.0;
+
+            // For MobX, since we're pulling the real values from a cache
+            final _ = viewModel.baseTrackHeight;
+
+            for (final (trackIndex, (trackId, isSendTrack))
+                in project.trackOrder
+                    .map((t) => (t, false))
+                    .followedBy(
+                      project.sendTrackOrder.reversed.map((t) => (t, true)),
+                    )
+                    .indexed) {
+              // For MobX, since we're pulling the real values from a cache
+              final _ = viewModel.trackHeightModifiers[trackId];
+
+              final trackPosition = viewModel.trackPositionCalculator
+                  .getTrackPosition(trackIndex);
+              final trackHeight = viewModel.trackPositionCalculator
+                  .getTrackHeight(trackIndex);
+
+              if (isSendTrack && !lastTrackWasSendTrack) {
+                lastTrackWasSendTrack = true;
+                positionForAddButton = lastTrackBottom;
+              }
+
+              lastTrackBottom = trackPosition + trackHeight;
+
+              if (trackPosition >= editorHeight) {
+                break;
+              }
+
+              if (trackPosition + trackHeight > 0) {
+                headers.add(
+                  Positioned(
+                    key: Key(trackId),
+                    top: trackPosition + (isSendTrack ? 1 : 0),
+                    left: 0,
+                    right: 0,
+                    child: SizedBox(
+                      height: trackHeight - 1,
+                      child: TrackHeader(trackID: trackId),
+                    ),
+                  ),
+                );
+                headers.add(
+                  Positioned(
+                    key: Key('$trackId-border'),
+                    top: trackPosition + (isSendTrack ? 0 : (trackHeight - 1)),
+                    left: 0,
+                    right: 0,
+                    height: 1,
+                    child: Container(color: AnthemTheme.panel.border),
+                  ),
+                );
+
+                const resizeHandleHeight = 10.0;
+
+                var resizeHandleTop =
+                    trackPosition - 1 - resizeHandleHeight / 2;
+                if (!isSendTrack) {
+                  resizeHandleTop += trackHeight + 1;
+                }
+
+                resizeHandles.add(
+                  Positioned(
+                    key: Key('$trackId-handle'),
+                    left: 0,
+                    right: 0,
+                    top: resizeHandleTop,
+                    child: TrackHeaderResizeHandle(
+                      resizeHandleHeight: resizeHandleHeight,
+                      trackHeight: trackHeight,
+                      isSendTrack: isSendTrack,
+                      trackId: trackId,
+                    ),
+                  ),
+                );
+              }
+            }
+
+            // If we didn't fill the whole area, then we are at the end of the
+            // track list, so we can add an "add track" button
+            if (!positionForAddButton.isNaN) {
+              headers.add(
+                Positioned(
+                  key: Key('add-track-button'),
+                  top: positionForAddButton + 8,
+                  left: 16,
+                  right: 16,
+                  child: Button(
+                    icon: Icons.add,
+                    hint: [.new('click', 'Add a new track')],
+                    onPress: () {
+                      final controller = ServiceRegistry.forProject(
+                        project.id,
+                      ).projectController;
+                      controller.addTrack();
+                    },
+                  ),
+                ),
+              );
+            }
+
+            return ClipRect(child: Stack(children: headers + resizeHandles));
+          },
+        );
+      },
     );
   }
 }
