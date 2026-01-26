828416b17681d3bc2cf2bb26214b449852e6de34
Joshua Wade <6700184+josh-audio@users.noreply.github.com>
2026-01-25 23:46:31 -0500

    Move track header widget to a separate file

---
+++ lib/widgets/editors/arranger/widgets/track_header.dart
@@ -0,0 +1,150 @@
+/*
+  Copyright (C) 2022 - 2026 Joshua Wade
+
+  This file is part of Anthem.
+
+  Anthem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Anthem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Anthem. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:anthem/helpers/id.dart';
+import 'package:anthem/logic/service_registry.dart';
+import 'package:anthem/model/project.dart';
+import 'package:anthem/theme.dart';
+import 'package:anthem/widgets/basic/menu/context_menu_api.dart';
+import 'package:anthem/widgets/basic/menu/menu_model.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+
+class TrackHeader extends StatelessObserverWidget {
+  final Id trackID;
+
+  const TrackHeader({super.key, required this.trackID});
+
+  @override
+  Widget build(BuildContext context) {
+    final project = Provider.of<ProjectModel>(context);
+    final projectServices = ServiceRegistry.forProject(project.id);
+    final projectController = projectServices.projectController;
+    final track = project.tracks[trackID]!;
+
+    final projectServiceRegistry = ServiceRegistry.forProject(project.id);
+    final controller = projectServiceRegistry.arrangerController;
+    final viewModel = projectServiceRegistry.arrangerViewModel;
+
+    final trackAnthemColor = track.color;
+    final colorShifter = trackAnthemColor.colorShifter;
+    final color = colorShifter.clipBase.toColor();
+
+    final trackBackgroundColor = viewModel.selectedTracks.contains(track.id)
+        ? AnthemTheme.panel.borderLight
+        : AnthemTheme.panel.main;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Observer(
+          builder: (context) {
+            return MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: GestureDetector(
+                onTap: () {
+                  if (HardwareKeyboard.instance.isShiftPressed) {
+                    controller.shiftClickToTrack(track.id);
+                    return;
+                  }
+
+                  if (HardwareKeyboard.instance.isControlPressed) {
+                    controller.toggleTrackSelection(track.id);
+                    return;
+                  }
+
+                  controller.selectTrack(track.id);
+                },
+                onSecondaryTapUp: (e) {
+                  if (!controller.isTrackSelected(track.id)) {
+                    controller.selectTrack(track.id);
+                  }
+
+                  openContextMenu(
+                    e.globalPosition,
+                    MenuDef(
+                      children: [
+                        AnthemMenuItem(
+                          text: 'Delete track',
+                          hint: 'Delete this track',
+                          onSelected: () {
+                            projectController.removeTrack(track.id);
+                            viewModel.selectedTracks.remove(track.id);
+                          },
+                        ),
+                        AnthemMenuItem(
+                          text: 'Delete selected tracks',
+                          hint: 'Delete the selected tracks',
+                          disabled: viewModel.selectedTracks.length <= 1,
+                          onSelected: () {
+                            projectController.removeTracks(
+                              viewModel.selectedTracks,
+                            );
+                            viewModel.selectedTracks.clear();
+                          },
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: Container(
+                  color: trackBackgroundColor,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: 9,
+                        decoration: BoxDecoration(
+                          color: color,
+                          border: Border(
+                            right: BorderSide(
+                              color: AnthemTheme.panel.border,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 10,
+                            vertical: 7,
+                          ),
+                          child: Text(
+                            track.name,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: AnthemTheme.text.main,
+                              fontSize: 11,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
--- lib/widgets/editors/arranger/widgets/track_headers.dart
+++ lib/widgets/editors/arranger/widgets/track_headers.dart
@@ -17,143 +17,21 @@
   along with Anthem. If not, see <https://www.gnu.org/licenses/>.
 */
 
-import 'package:anthem/helpers/id.dart';
 import 'package:anthem/logic/service_registry.dart';
 import 'package:anthem/model/project.dart';
 import 'package:anthem/theme.dart';
 import 'package:anthem/widgets/basic/button.dart';
 import 'package:anthem/widgets/basic/hint/hint.dart';
 import 'package:anthem/widgets/basic/icon.dart';
-import 'package:anthem/widgets/basic/menu/context_menu_api.dart';
 import 'package:anthem/widgets/basic/menu/menu.dart';
 import 'package:anthem/widgets/basic/menu/menu_model.dart';
 import 'package:anthem/widgets/editors/arranger/helpers.dart';
 import 'package:anthem/widgets/editors/arranger/view_model.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 
-class _TrackHeader extends StatelessObserverWidget {
-  final Id trackID;
-
-  const _TrackHeader({required this.trackID});
-
-  @override
-  Widget build(BuildContext context) {
-    final project = Provider.of<ProjectModel>(context);
-    final projectServices = ServiceRegistry.forProject(project.id);
-    final projectController = projectServices.projectController;
-    final track = project.tracks[trackID]!;
-
-    final projectServiceRegistry = ServiceRegistry.forProject(project.id);
-    final controller = projectServiceRegistry.arrangerController;
-    final viewModel = projectServiceRegistry.arrangerViewModel;
-
-    final trackAnthemColor = track.color;
-    final colorShifter = trackAnthemColor.colorShifter;
-    final color = colorShifter.clipBase.toColor();
-
-    final trackBackgroundColor = viewModel.selectedTracks.contains(track.id)
-        ? AnthemTheme.panel.borderLight
-        : AnthemTheme.panel.main;
-
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Observer(
-          builder: (context) {
-            return MouseRegion(
-              cursor: SystemMouseCursors.click,
-              child: GestureDetector(
-                onTap: () {
-                  if (HardwareKeyboard.instance.isShiftPressed) {
-                    controller.shiftClickToTrack(track.id);
-                    return;
-                  }
-
-                  if (HardwareKeyboard.instance.isControlPressed) {
-                    controller.toggleTrackSelection(track.id);
-                    return;
-                  }
-
-                  controller.selectTrack(track.id);
-                },
-                onSecondaryTapUp: (e) {
-                  if (!controller.isTrackSelected(track.id)) {
-                    controller.selectTrack(track.id);
-                  }
-
-                  openContextMenu(
-                    e.globalPosition,
-                    MenuDef(
-                      children: [
-                        AnthemMenuItem(
-                          text: 'Delete track',
-                          hint: 'Delete this track',
-                          onSelected: () {
-                            projectController.removeTrack(track.id);
-                            viewModel.selectedTracks.remove(track.id);
-                          },
-                        ),
-                        AnthemMenuItem(
-                          text: 'Delete selected tracks',
-                          hint: 'Delete the selected tracks',
-                          disabled: viewModel.selectedTracks.length <= 1,
-                          onSelected: () {
-                            projectController.removeTracks(
-                              viewModel.selectedTracks,
-                            );
-                            viewModel.selectedTracks.clear();
-                          },
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                child: Container(
-                  color: trackBackgroundColor,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        width: 9,
-                        decoration: BoxDecoration(
-                          color: color,
-                          border: Border(
-                            right: BorderSide(
-                              color: AnthemTheme.panel.border,
-                              width: 1,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 10,
-                            vertical: 7,
-                          ),
-                          child: Text(
-                            track.name,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              color: AnthemTheme.text.main,
-                              fontSize: 11,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
-}
+import 'track_header.dart';
 
 class _TrackHeaderResizeHandle extends StatefulObserverWidget {
   final double resizeHandleHeight;
@@ -411,7 +289,7 @@ class _TrackHeadersState extends State<TrackHeaders> {
                     right: 0,
                     child: SizedBox(
                       height: trackHeight - 1,
-                      child: _TrackHeader(trackID: trackId),
+                      child: TrackHeader(trackID: trackId),
                     ),
                   ),
                 );
