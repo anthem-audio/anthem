4bfad1503e1aefe268d70fcbbf5fe91a44631b8a
Joshua Wade <6700184+josh-audio@users.noreply.github.com>
2026-02-17 22:55:19 -0500

    Add new slider widget

---
+++ lib/widgets/basic/controls/slider.dart
@@ -0,0 +1,500 @@
+/*
+  Copyright (C) 2026 Joshua Wade
+
+  This file is part of Anthem.
+
+  Anthem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Anthem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Anthem. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'dart:math';
+
+import 'package:anthem/theme.dart';
+import 'package:anthem/widgets/basic/hint/hint_store.dart';
+import 'package:anthem/widgets/util/lazy_follower.dart';
+import 'package:flutter/widgets.dart';
+
+import 'control_mouse_handler.dart';
+
+const _stickyTrapSize = 0.08;
+
+class Slider extends StatefulWidget {
+  final double? width;
+  final double? height;
+  final SliderAxis axis;
+  final SliderType type;
+  final double borderRadius;
+
+  final double value;
+  final double min;
+  final double max;
+
+  final void Function(double)? onValueChanged;
+
+  final List<double> stickyPoints;
+
+  final String Function(double value)? hoverHintOverride;
+  final String Function(double value)? hint;
+
+  const Slider({
+    super.key,
+    this.width,
+    this.height,
+    this.axis = SliderAxis.horizontal,
+    this.type = SliderType.normal,
+    this.borderRadius = 1,
+    required this.value,
+    this.onValueChanged,
+    this.max = 1,
+    double? min,
+    this.stickyPoints = const [],
+    this.hoverHintOverride,
+    this.hint,
+  }) : min = min ?? (type == SliderType.pan ? -1 : 0),
+       assert(borderRadius >= 0);
+
+  @override
+  State<Slider> createState() => _SliderState();
+}
+
+class _SliderState extends State<Slider> with TickerProviderStateMixin {
+  LazyFollowAnimationHelper? animationHelper;
+
+  bool isOver = false;
+  bool isPressed = false;
+
+  double lastValue = -1;
+
+  double scaledToRaw(double value) =>
+      (value - widget.min) / (widget.max - widget.min);
+
+  double? stickyTrapCounter;
+
+  double pastStart = 0;
+  double pastEnd = 0;
+
+  int? currentHintId;
+
+  void setHint({required bool hover}) {
+    String? currentHintText;
+    if (hover && widget.hoverHintOverride != null) {
+      currentHintText = widget.hoverHintOverride!.call(lastValue);
+    } else if (widget.hint != null) {
+      currentHintText = widget.hint!.call(lastValue);
+    }
+
+    if (currentHintText == null) {
+      clearHint();
+      return;
+    }
+
+    if (currentHintId == null) {
+      currentHintId = HintStore.instance.addHint([
+        HintSection('click + drag', currentHintText),
+      ]);
+    } else {
+      HintStore.instance.updateHint(currentHintId!, [
+        HintSection('click + drag', currentHintText),
+      ]);
+    }
+  }
+
+  void clearHint() {
+    if (currentHintId != null) {
+      HintStore.instance.removeHint(currentHintId!);
+      currentHintId = null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    animationHelper ??= LazyFollowAnimationHelper(
+      duration: 250,
+      vsync: this,
+      items: [
+        // Handle thickness
+        LazyFollowItem(initialValue: 1),
+        // Handle press color blend amount
+        LazyFollowItem(initialValue: 0),
+      ],
+    );
+
+    void setHoverAnimationState(bool hover) {
+      final [handleSizeHelper, _] = animationHelper!.items;
+      handleSizeHelper.setTarget(hover ? 3 : 1);
+    }
+
+    void setPressAnimationState(bool pressed) {
+      final [_, pressColorHelper] = animationHelper!.items;
+      pressColorHelper.setTarget(pressed ? 1 : 0);
+    }
+
+    return MouseRegion(
+      onEnter: (e) {
+        setState(() {
+          isOver = true;
+        });
+
+        lastValue = widget.value;
+        setHint(hover: true);
+
+        setHoverAnimationState(true);
+        animationHelper!.update();
+      },
+      onExit: (e) {
+        setState(() {
+          isOver = false;
+        });
+
+        clearHint();
+
+        if (!isPressed) {
+          setHoverAnimationState(false);
+          animationHelper!.update();
+        }
+      },
+      child: ControlMouseHandler(
+        cursor: switch (widget.axis) {
+          SliderAxis.horizontal => SystemMouseCursors.resizeLeftRight,
+          SliderAxis.vertical => SystemMouseCursors.resizeUpDown,
+        },
+        onStart: () {
+          setState(() {
+            isPressed = true;
+          });
+
+          pastStart = 0;
+          pastEnd = 0;
+          stickyTrapCounter = null;
+
+          lastValue = widget.value;
+          setHint(hover: false);
+
+          setPressAnimationState(true);
+          animationHelper!.update();
+        },
+        onEnd: (e) {
+          setState(() {
+            isPressed = false;
+          });
+
+          setPressAnimationState(false);
+
+          if (!isOver) {
+            setHoverAnimationState(false);
+          }
+
+          animationHelper!.update();
+        },
+        onChange: (e) {
+          if (widget.onValueChanged == null) return;
+
+          final rawPixelChange = switch (widget.axis) {
+            SliderAxis.horizontal => e.delta.dx,
+            SliderAxis.vertical => e.delta.dy,
+          };
+
+          var valueChange = rawPixelChange / 300;
+
+          // Handle existing overshoot past beginning
+          if (pastStart < 0) {
+            pastStart += valueChange;
+            if (pastStart > 0) {
+              valueChange = pastStart;
+              pastStart = 0;
+            } else {
+              valueChange = 0;
+            }
+          }
+
+          // Handle existing overshoot past end
+          if (pastEnd > 0) {
+            pastEnd += valueChange;
+            if (pastEnd < 0) {
+              valueChange = pastEnd;
+              pastEnd = 0;
+            } else {
+              valueChange = 0;
+            }
+          }
+
+          if (valueChange == 0) return;
+
+          // Handle new overshoot past beginning
+          final lastValueRaw = scaledToRaw(lastValue);
+          if (lastValueRaw + valueChange < 0) {
+            pastStart += lastValueRaw + valueChange;
+            valueChange = -lastValueRaw;
+          }
+
+          // Handle new overshoot past end
+          if (lastValueRaw + valueChange > 1) {
+            pastEnd += lastValueRaw + valueChange - 1;
+            valueChange = 1 - lastValueRaw;
+          }
+
+          var newValueRaw = lastValueRaw + valueChange;
+
+          if (pastEnd > 0) {
+            newValueRaw = 1;
+          } else if (pastStart < 0) {
+            newValueRaw = 0;
+          }
+
+          final newValue = newValueRaw * (widget.max - widget.min) + widget.min;
+
+          if (stickyTrapCounter == null) {
+            double? stickyValue;
+
+            // Check if this change crosses one of the sticky points
+            for (final stickyPoint in widget.stickyPoints) {
+              if (lastValue < stickyPoint && newValue >= stickyPoint) {
+                stickyTrapCounter = -_stickyTrapSize;
+                stickyValue = stickyPoint;
+                break;
+              } else if (lastValue > stickyPoint && newValue <= stickyPoint) {
+                stickyTrapCounter = _stickyTrapSize;
+                stickyValue = stickyPoint;
+                break;
+              }
+            }
+
+            widget.onValueChanged?.call(stickyValue ?? newValue);
+            lastValue = stickyValue ?? newValue;
+          } else {
+            stickyTrapCounter = stickyTrapCounter! + valueChange;
+
+            if (stickyTrapCounter!.abs() > _stickyTrapSize) {
+              final overshoot = stickyTrapCounter! > 0
+                  ? stickyTrapCounter! - _stickyTrapSize
+                  : stickyTrapCounter! + _stickyTrapSize;
+
+              final newValue =
+                  (scaledToRaw(lastValue) + overshoot) *
+                      (widget.max - widget.min) +
+                  widget.min;
+
+              widget.onValueChanged?.call(newValue);
+
+              stickyTrapCounter = null;
+              lastValue = newValue;
+            }
+          }
+
+          setHint(hover: false);
+        },
+        child: SizedBox(
+          width: widget.width,
+          height: widget.height,
+          child: AnimatedBuilder(
+            animation: animationHelper!.animationController,
+            builder: (context, _) {
+              final [handleSizeHelper, pressColorHelper] =
+                  animationHelper!.items;
+
+              return CustomPaint(
+                painter: _SliderPainter(
+                  value: scaledToRaw(widget.value),
+                  axis: widget.axis,
+                  type: widget.type,
+                  handleThickness: handleSizeHelper.animation.value,
+                  handlePressAmount: pressColorHelper.animation.value,
+                  borderRadius: widget.borderRadius,
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    clearHint();
+    animationHelper?.dispose();
+    super.dispose();
+  }
+}
+
+class _SliderPainter extends CustomPainter {
+  final double value;
+  final SliderAxis axis;
+  final SliderType type;
+  final double handleThickness;
+  final double handlePressAmount;
+  final double borderRadius;
+
+  const _SliderPainter({
+    required this.value,
+    required this.axis,
+    required this.type,
+    required this.handleThickness,
+    required this.handlePressAmount,
+    required this.borderRadius,
+  });
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    if (size.width <= 1 || size.height <= 1) {
+      return;
+    }
+
+    final clampedValue = value.clamp(0.0, 1.0).toDouble();
+
+    final trackBackgroundPaint = Paint()
+      ..color = AnthemTheme.control.background
+      ..style = PaintingStyle.fill;
+    final trackBorderPaint = Paint()
+      ..color = AnthemTheme.panel.border
+      ..strokeWidth = 1
+      ..style = PaintingStyle.stroke;
+    final activeFillPaint = Paint()
+      ..color = AnthemTheme.control.activeBackground
+      ..style = PaintingStyle.fill;
+    final handlePaint = Paint()
+      ..color = Color.lerp(
+        AnthemTheme.control.active,
+        AnthemTheme.control.activePressed,
+        handlePressAmount.clamp(0.0, 1.0),
+      )!
+      ..style = PaintingStyle.fill;
+
+    final borderRect = Rect.fromLTWH(0.5, 0.5, size.width - 1, size.height - 1);
+    final innerRect = Rect.fromLTWH(1, 1, size.width - 2, size.height - 2);
+    final trackRadius = Radius.circular(borderRadius);
+    final innerContentRadius = Radius.circular(max(0, max(borderRadius - 1, 1)));
+    final borderRRect = RRect.fromRectAndRadius(borderRect, trackRadius);
+    final innerContentClipRRect = RRect.fromRectAndRadius(
+      innerRect,
+      innerContentRadius,
+    );
+
+    final handleCenter = switch (axis) {
+      SliderAxis.horizontal => Offset(
+        borderRect.left + 1 + (borderRect.width - 2) * clampedValue,
+        borderRect.center.dy,
+      ),
+      SliderAxis.vertical => Offset(
+        borderRect.center.dx,
+        (borderRect.bottom + 1) - (borderRect.height - 2) * clampedValue,
+      ),
+    };
+
+    canvas.save();
+    canvas.clipRRect(innerContentClipRRect);
+
+    canvas.drawRect(innerRect, trackBackgroundPaint);
+
+    if (axis == SliderAxis.horizontal) {
+      if (type == SliderType.normal) {
+        final width = (handleCenter.dx - innerRect.left).clamp(
+          0.0,
+          innerRect.width,
+        );
+        if (width > 0) {
+          final rect = Rect.fromLTWH(
+            innerRect.left,
+            innerRect.top,
+            width,
+            innerRect.height,
+          );
+          canvas.drawRect(rect, activeFillPaint);
+        }
+      } else {
+        final centerX = innerRect.center.dx;
+        final clampedHandleX = handleCenter.dx.clamp(
+          innerRect.left,
+          innerRect.right,
+        );
+        final left = min(centerX, clampedHandleX);
+        final width = (centerX - clampedHandleX).abs();
+
+        if (width > 0) {
+          final rect = Rect.fromLTWH(
+            left,
+            innerRect.top,
+            width,
+            innerRect.height,
+          );
+          canvas.drawRect(rect, activeFillPaint);
+        }
+      }
+    } else {
+      if (type == SliderType.normal) {
+        final height = (innerRect.bottom - handleCenter.dy).clamp(
+          0.0,
+          innerRect.height,
+        );
+        if (height > 0) {
+          final rect = Rect.fromLTWH(
+            innerRect.left,
+            innerRect.bottom - height,
+            innerRect.width,
+            height,
+          );
+          canvas.drawRect(rect, activeFillPaint);
+        }
+      } else {
+        final centerY = innerRect.center.dy;
+        final clampedHandleY = handleCenter.dy.clamp(
+          innerRect.top,
+          innerRect.bottom,
+        );
+        final top = min(centerY, clampedHandleY);
+        final height = (centerY - clampedHandleY).abs();
+
+        if (height > 0) {
+          final rect = Rect.fromLTWH(
+            innerRect.left,
+            top,
+            innerRect.width,
+            height,
+          );
+          canvas.drawRect(rect, activeFillPaint);
+        }
+      }
+    }
+
+    final handleRect = switch (axis) {
+      SliderAxis.horizontal => Rect.fromCenter(
+        center: handleCenter,
+        width: handleThickness,
+        height: innerRect.height - 0.5,
+      ),
+      SliderAxis.vertical => Rect.fromCenter(
+        center: handleCenter,
+        width: innerRect.width,
+        height: handleThickness,
+      ),
+    };
+    canvas.drawRect(handleRect, handlePaint);
+    canvas.restore();
+
+    canvas.drawRRect(borderRRect, trackBorderPaint);
+  }
+
+  @override
+  bool shouldRepaint(covariant _SliderPainter oldDelegate) {
+    return oldDelegate.value != value ||
+        oldDelegate.axis != axis ||
+        oldDelegate.type != type ||
+        oldDelegate.handleThickness != handleThickness ||
+        oldDelegate.handlePressAmount != handlePressAmount ||
+        oldDelegate.borderRadius != borderRadius;
+  }
+}
+
+enum SliderType { normal, pan }
+
+enum SliderAxis { horizontal, vertical }
