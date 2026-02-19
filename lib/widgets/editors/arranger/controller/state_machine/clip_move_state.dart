753bb62840de1d92f18c6d6ed0372f26ace6765a
Joshua Wade <6700184+josh-audio@users.noreply.github.com>
2026-02-18 23:57:42 -0500

    Break arranger state machine into parts

---
+++ lib/widgets/editors/arranger/controller/state_machine/clip_move_state.dart
@@ -0,0 +1,309 @@
+/*
+  Copyright (C) 2026 Joshua Wade
+
+  This file is part of Anthem.
+
+  Anthem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Anthem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Anthem. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+part of 'arranger_state_machine.dart';
+
+class ArrangerClipMoveState
+    extends EditorStateMachineState<ArrangerStateMachineData> {
+  ArrangerStateMachine get arrangerStateMachine =>
+      stateMachine as ArrangerStateMachine;
+
+  ArrangerStateMachineData get interactionState => arrangerStateMachine.data;
+
+  ProjectModel get project => arrangerStateMachine.project;
+  ArrangerViewModel get viewModel => arrangerStateMachine.viewModel;
+
+  @override
+  ArrangerDragState get parentState => super.parentState as ArrangerDragState;
+
+  /// The IDs of clips that are being moved by this operation.
+  Set<Id>? _movingClipIds;
+
+  /// At the start of the drag, this represents the distance between the
+  /// left-most selected clip and the start of the arrangement.
+  ///
+  /// This is calculated because we cannot move clips any further than this,
+  /// otherwise at least one of them would start before the start of the
+  /// arrangement.
+  int _minimumMoveDelta = 0;
+
+  @override
+  Iterable<EditorStateMachineStateTransition<ArrangerStateMachineData>>
+  get transitions => [
+    .new(
+      name: 'Delegate drag to clip move',
+      from: ArrangerDragState,
+      to: ArrangerClipMoveState,
+      canTransition: ({required data, required event, required currentState}) =>
+          (currentState as ArrangerDragState).shouldDelegateToClipMove,
+    ),
+    .new(
+      name: 'Cancel clip move',
+      from: ArrangerClipMoveState,
+      to: ArrangerDragState,
+      canTransition: ({required data, required event, required currentState}) =>
+          isArrangerCancelSignal(event),
+    ),
+    .new(
+      name: 'Clip move fallback to drag',
+      from: ArrangerClipMoveState,
+      to: ArrangerDragState,
+      canTransition: ({required data, required event, required currentState}) =>
+          !(currentState as ArrangerClipMoveState)
+              .parentState
+              .isDragPointerActive,
+    ),
+  ];
+
+  ArrangerClipMoveState(super.parentState);
+
+  @override
+  void onEntry({required event, required from}) {
+    _initializeMoveSession();
+    _syncClipOverrides();
+  }
+
+  @override
+  void onActive({required event}) {
+    _syncClipOverrides();
+  }
+
+  @override
+  void onExit({required event, required to}) {
+    _commitMoveSessionIfNeeded(event: event);
+    _clearMoveSession();
+  }
+
+  void _commitMoveSessionIfNeeded({required EditorStateMachineEvent event}) {
+    if (isArrangerCancelSignal(event)) {
+      return;
+    }
+
+    if (event is! EditorStateMachineSignalEvent) {
+      return;
+    }
+
+    final signal = event.signal;
+    if (signal is! _ArrangerPointerUpSignal ||
+        signal.event is PointerCancelEvent) {
+      return;
+    }
+
+    final movingClipIds = _movingClipIds;
+    final arrangementId = project.sequence.activeArrangementID;
+    if (movingClipIds == null || arrangementId == null) {
+      return;
+    }
+
+    final arrangement = project.sequence.arrangements[arrangementId];
+    if (arrangement == null) {
+      return;
+    }
+
+    final arrangementClips = arrangement.clips.nonObservableInner;
+    final clipTimingOverrides =
+        viewModel.clipTimingOverrides.nonObservableInner;
+    final clipMoves = <({Id clipID, int oldOffset, int newOffset})>[];
+
+    for (final clipId in movingClipIds) {
+      final clip = arrangementClips[clipId];
+      final clipTimingOverride = clipTimingOverrides[clipId];
+      if (clip == null || clipTimingOverride == null) {
+        continue;
+      }
+
+      final oldOffset = clip.offset;
+      final newOffset = clipTimingOverride.offset;
+      if (oldOffset == newOffset) {
+        continue;
+      }
+
+      clipMoves.add((
+        clipID: clip.id,
+        oldOffset: oldOffset,
+        newOffset: newOffset,
+      ));
+    }
+
+    if (clipMoves.isEmpty) {
+      return;
+    }
+
+    project.execute(
+      MoveClipsCommand(arrangementID: arrangement.id, clipMoves: clipMoves),
+    );
+  }
+
+  void _initializeMoveSession() {
+    _movingClipIds = null;
+    _minimumMoveDelta = 0;
+    final clipTimingOverrides = viewModel.clipTimingOverrides;
+    clipTimingOverrides.clear();
+
+    final arrangementId = project.sequence.activeArrangementID;
+    if (arrangementId == null) {
+      return;
+    }
+
+    final arrangement = project.sequence.arrangements[arrangementId];
+    if (arrangement == null) {
+      return;
+    }
+    final arrangementClips = arrangement.clips.nonObservableInner;
+
+    final pressedClipId = parentState.dragStartClipId;
+    if (pressedClipId == null) {
+      return;
+    }
+
+    final pressedClip = arrangementClips[pressedClipId];
+    if (pressedClip == null) {
+      return;
+    }
+
+    viewModel.pressedClip = pressedClip.id;
+
+    final selectedClips = viewModel.selectedClips;
+    var selectedClipIds = selectedClips.nonObservableInner;
+    if (!selectedClipIds.contains(pressedClip.id)) {
+      selectedClips.clear();
+      selectedClipIds = selectedClips.nonObservableInner;
+    }
+
+    final movingClipIds = selectedClipIds.contains(pressedClip.id)
+        ? selectedClipIds.toSet()
+        : <Id>{pressedClip.id};
+    _movingClipIds = Set<Id>.unmodifiable(movingClipIds);
+
+    int? smallestStartOffset;
+    var hasAnyOverrides = false;
+
+    for (final clipId in _movingClipIds!) {
+      final clip = arrangementClips[clipId];
+      if (clip == null) {
+        continue;
+      }
+      hasAnyOverrides = true;
+
+      final timeViewStart = clip.timeView?.start ?? 0;
+      final timeViewEnd = clip.timeView?.end ?? clip.width;
+
+      clipTimingOverrides[clip.id] = ClipTimingOverride(
+        offset: clip.offset,
+        timeViewStart: timeViewStart,
+        timeViewEnd: timeViewEnd,
+      );
+
+      if (smallestStartOffset == null || clip.offset < smallestStartOffset) {
+        smallestStartOffset = clip.offset;
+      }
+    }
+
+    if (!hasAnyOverrides) {
+      _movingClipIds = null;
+      return;
+    }
+
+    _minimumMoveDelta = -(smallestStartOffset ?? 0);
+  }
+
+  void _syncClipOverrides() {
+    final movingClipIds = _movingClipIds;
+    final dragStartPosition = parentState.dragStartPosition;
+    final dragCurrentPosition = parentState.dragCurrentPosition;
+    if (movingClipIds == null ||
+        dragStartPosition == null ||
+        dragCurrentPosition == null) {
+      return;
+    }
+
+    final arrangementId = project.sequence.activeArrangementID;
+    if (arrangementId == null) {
+      return;
+    }
+
+    final arrangement = project.sequence.arrangements[arrangementId];
+    if (arrangement == null) {
+      return;
+    }
+    final arrangementClips = arrangement.clips.nonObservableInner;
+
+    final startTime = pixelsToTime(
+      timeViewStart: interactionState.renderedTimeViewStart,
+      timeViewEnd: interactionState.renderedTimeViewEnd,
+      viewPixelWidth: interactionState.viewSize.width,
+      pixelOffsetFromLeft: dragStartPosition.x,
+    );
+    final currentTime = pixelsToTime(
+      timeViewStart: interactionState.renderedTimeViewStart,
+      timeViewEnd: interactionState.renderedTimeViewEnd,
+      viewPixelWidth: interactionState.viewSize.width,
+      pixelOffsetFromLeft: dragCurrentPosition.x,
+    );
+
+    var movedDistance = (currentTime - startTime).round();
+
+    if (!interactionState.isAltPressed) {
+      movedDistance = getSnappedTime(
+        rawTime: movedDistance,
+        divisionChanges: arrangerStateMachine.divisionChanges(),
+        round: true,
+      );
+    }
+
+    if (movedDistance < _minimumMoveDelta) {
+      movedDistance = _minimumMoveDelta;
+    }
+
+    final clipTimingOverrides = viewModel.clipTimingOverrides;
+
+    for (final clipId in movingClipIds) {
+      final clip = arrangementClips[clipId];
+      if (clip == null) {
+        clipTimingOverrides.remove(clipId);
+        continue;
+      }
+
+      final timeViewStart = clip.timeView?.start ?? 0;
+      final timeViewEnd = clip.timeView?.end ?? clip.width;
+      final nextOffset = clip.offset + movedDistance;
+
+      final currentOverride = clipTimingOverrides.nonObservableInner[clip.id];
+      if (currentOverride != null &&
+          currentOverride.offset == nextOffset &&
+          currentOverride.timeViewStart == timeViewStart &&
+          currentOverride.timeViewEnd == timeViewEnd) {
+        continue;
+      }
+
+      clipTimingOverrides[clip.id] = ClipTimingOverride(
+        offset: nextOffset,
+        timeViewStart: timeViewStart,
+        timeViewEnd: timeViewEnd,
+      );
+    }
+  }
+
+  void _clearMoveSession() {
+    _movingClipIds = null;
+    _minimumMoveDelta = 0;
+    viewModel.clipTimingOverrides.clear();
+    viewModel.pressedClip = null;
+  }
+}
---
+++ lib/widgets/editors/arranger/controller/state_machine/selection_box_state.dart
@@ -0,0 +1,194 @@
+/*
+  Copyright (C) 2026 Joshua Wade
+
+  This file is part of Anthem.
+
+  Anthem is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Anthem is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Anthem. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+part of 'arranger_state_machine.dart';
+
+class ArrangerSelectionBoxState
+    extends EditorStateMachineState<ArrangerStateMachineData> {
+  ArrangerStateMachine get arrangerStateMachine =>
+      stateMachine as ArrangerStateMachine;
+
+  ArrangerStateMachineData get interactionState => arrangerStateMachine.data;
+
+  ArrangerViewModel get viewModel => arrangerStateMachine.viewModel;
+
+  @override
+  ArrangerDragState get parentState => super.parentState as ArrangerDragState;
+
+  Set<Id>? _originalSelectedClipsAtEntry;
+  bool _isSubtractiveSelectionLatched = false;
+
+  @visibleForTesting
+  Set<Id>? get originalSelectedClipsAtEntry => _originalSelectedClipsAtEntry;
+
+  @visibleForTesting
+  bool get isSubtractiveSelectionLatched => _isSubtractiveSelectionLatched;
+
+  Rectangle<double>? _getSelectionBoxRect() {
+    final startPosition = parentState.dragStartPosition;
+    final currentPosition = parentState.dragCurrentPosition;
+    if (startPosition == null || currentPosition == null) {
+      return null;
+    }
+
+    return Rectangle.fromPoints(
+      Point(startPosition.x, startPosition.y),
+      Point(currentPosition.x, currentPosition.y),
+    );
+  }
+
+  void _syncSelectionBox() {
+    viewModel.selectionBox = _getSelectionBoxRect();
+  }
+
+  Id? _getClipAtDragStart() {
+    final startPosition = parentState.dragStartPosition;
+    if (startPosition == null) {
+      return null;
+    }
+
+    final contentUnderCursor = viewModel.getContentUnderCursor(
+      Offset(startPosition.x, startPosition.y),
+    );
+
+    return contentUnderCursor.clip?.metadata ??
+        contentUnderCursor.resizeHandle?.metadata.id;
+  }
+
+  void _initializeSelectionSession() {
+    if (!interactionState.isShiftPressed) {
+      viewModel.selectedClips.clear();
+      _originalSelectedClipsAtEntry = Set<Id>.unmodifiable(const <Id>{});
+      _isSubtractiveSelectionLatched = false;
+      return;
+    }
+
+    _originalSelectedClipsAtEntry = Set<Id>.unmodifiable(
+      viewModel.selectedClips.nonObservableInner,
+    );
+
+    final clipAtDragStart = _getClipAtDragStart();
+    _isSubtractiveSelectionLatched =
+        clipAtDragStart != null &&
+        _originalSelectedClipsAtEntry!.contains(clipAtDragStart);
+  }
+
+  void _clearSelectionSession() {
+    _originalSelectedClipsAtEntry = null;
+    _isSubtractiveSelectionLatched = false;
+  }
+
+  Set<Id> _getClipsInSelectionBox({required Rectangle<double> selectionBox}) {
+    if (selectionBox.width <= 0 || selectionBox.height <= 0) {
+      return {};
+    }
+
+    final selectionRect = Rect.fromLTWH(
+      selectionBox.left,
+      selectionBox.top,
+      selectionBox.width,
+      selectionBox.height,
+    );
+
+    final clipsInSelection = <Id>{};
+    for (final annotation in viewModel.visibleClips.getAnnotations()) {
+      if (selectionRect.overlaps(annotation.rect)) {
+        clipsInSelection.add(annotation.metadata);
+      }
+    }
+
+    return clipsInSelection;
+  }
+
+  void _syncSelectedClips() {
+    final originalSelectedClips = _originalSelectedClipsAtEntry;
+    final selectionBox = viewModel.selectionBox;
+    if (originalSelectedClips == null || selectionBox == null) {
+      return;
+    }
+
+    final clipsInSelection = _getClipsInSelectionBox(
+      selectionBox: selectionBox,
+    );
+    final nextSelection = _isSubtractiveSelectionLatched
+        ? originalSelectedClips.difference(clipsInSelection)
+        : originalSelectedClips.union(clipsInSelection);
+
+    viewModel.selectedClips
+      ..clear()
+      ..addAll(nextSelection);
+  }
+
+  @override
+  Iterable<EditorStateMachineStateTransition<ArrangerStateMachineData>>
+  get transitions => [
+    .new(
+      name: 'Delegate drag to selection box',
+      from: ArrangerDragState,
+      to: ArrangerSelectionBoxState,
+      canTransition: ({required data, required event, required currentState}) =>
+          (currentState as ArrangerDragState).shouldDelegateToSelectionBox,
+    ),
+    .new(
+      name: 'Cancel selection box',
+      from: ArrangerSelectionBoxState,
+      to: ArrangerDragState,
+      canTransition: ({required data, required event, required currentState}) =>
+          isArrangerCancelSignal(event),
+    ),
+    .new(
+      name: 'Selection box fallback to drag',
+      from: ArrangerSelectionBoxState,
+      to: ArrangerDragState,
+      canTransition: ({required data, required event, required currentState}) =>
+          !(currentState as ArrangerSelectionBoxState)
+              .parentState
+              .isDragPointerActive,
+    ),
+  ];
+
+  ArrangerSelectionBoxState(super.parentState);
+
+  @override
+  void onEntry({required event, required from}) {
+    viewModel.cursorLocation = null;
+    _initializeSelectionSession();
+    _syncSelectionBox();
+    _syncSelectedClips();
+  }
+
+  @override
+  void onActive({required event}) {
+    _syncSelectionBox();
+    _syncSelectedClips();
+  }
+
+  @override
+  void onExit({required event, required to}) {
+    final originalSelectedClips = _originalSelectedClipsAtEntry;
+    if (isArrangerCancelSignal(event) && originalSelectedClips != null) {
+      viewModel.selectedClips
+        ..clear()
+        ..addAll(originalSelectedClips);
+    }
+
+    viewModel.selectionBox = null;
+    _clearSelectionSession();
+  }
+}
